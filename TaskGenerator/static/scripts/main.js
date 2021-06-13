b2b78f00b7e2b232ec618d16ffea41a773474738
Mushfiqur Rahman <iam1mahin@gmail.com>
2021-06-13 18:34:03 +0600

    index.html File

--- TaskGenerator/static/scripts/main.js
+++ TaskGenerator/static/scripts/main.js
@@ -6,15 +6,27 @@ ____ ________ ---- ________
 Verb Activity      Subject
 
 */
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    // true for mobile device
+    $("#taskCard").innerHTML = "📱 Shake 📱";
+}
+
 const shakeEvent = new Shake({ threshold: 15 });
 
 shakeEvent.start();
 
 window.addEventListener('shake', shaked, false);
 
+window.onkeypress = function(ev) {
+    if (ev.code == "Space") {
+        shaked();
+    }
+}
+
 function shaked() {
     document.getElementById('task').innerHTML = generate();
-    
+
     $("#taskCard").effect("shake", { direction: "right", times: 3, distance: 10 });
     // alert(generate());
 }
